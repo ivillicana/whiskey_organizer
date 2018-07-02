7eb169108798df12e0e534ddee696a6847cf08d6
Isaac Villicana <villicana.i@gmail.com>
2018-07-02 19:16:36 -0400

    add users/:slug to UserController and create corresponding view

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,7 @@
 class UserController < ApplicationController
-
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
+        @session = session
+        erb :'users/home'
+    end
 end
