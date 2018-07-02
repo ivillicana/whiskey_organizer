d327f4a26150ee9e0b8e08b91083e30164c40e13
Isaac Villicana <villicana.i@gmail.com>
2018-07-02 17:56:28 -0400

    update model associations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
     belongs_to :user
+    has_many :whiskey_items
+    has_many :whiskeys, through: :whiskey_items
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-    has_many :whiskeys
     has_many :items
+    has_many :whiskeys, through: :items
 
     has_secure_password
 end
