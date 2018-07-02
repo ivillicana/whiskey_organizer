6f0593b3a47f57dc85451ad63541785f7d327f89
Isaac Villicana <villicana.i@gmail.com>
2018-07-02 18:43:33 -0400

    add #slug and self.find_by_slug to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
     has_many :whiskeys, through: :items
 
     has_secure_password
+
+    def slug
+        self.username.gsub(" ", "-").downcase
+    end
+
+    def self.find_by_slug(arg)
+        self.all.find {|user| user.slug == arg}
+    end
 end
