43e40572e4fe04fa171241328f0cfeb36c8e084c
Isaac Villicana <villicana.i@gmail.com>
2018-07-02 16:41:31 -0400

    create whiskeys table

---
+++ db/migrate/20180702202730_create_whiskeys.rb
@@ -0,0 +1,9 @@
+class CreateWhiskeys < ActiveRecord::Migration[5.2]
+  def change
+    create_table :whiskeys do |t|
+      t.string :brand
+      t.string :name
+      t.string :type
+    end
+  end
+end
