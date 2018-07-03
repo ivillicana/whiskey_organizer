6e557bd4ef6d7e65269a0978a7facbecdbef7d27
Isaac Villicana <villicana.i@gmail.com>
2018-07-02 20:57:01 -0400

    add signup route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,7 +1,36 @@
 class UserController < ApplicationController
+
     get '/users/:slug' do
         @user = User.find_by_slug(params[:slug])
         @session = session
         erb :'users/home'
     end
+
+    get '/signup' do
+        if Helper.logged_in?(session)
+            user = User.find_by(s)
+            redirect "/users/#{User.find(session[:user_id]).slug}"
+        else
+            erb :'users/signup'
+        end
+    end
+
+    post '/signup' do
+        if params[:user].all? {|k,v| !k.empty?}
+            user = User.create(params[:user])
+            session[:user_id] = user.id 
+            redirect "/users/#{user.slug}"
+        else
+            redirect '/signup'
+        end
+    end
+
+    get '/login' do
+        if Helper.logged_in?(session)
+            redirect "/users/#{User.find(session[:user_id]).slug}"
+        else
+            erb :'users/login'
+        end
+    end
+
 end
