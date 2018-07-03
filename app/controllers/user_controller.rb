24bce1feaa6b7e3a67cfc5555cfc2eff28d79b70
Isaac Villicana <villicana.i@gmail.com>
2018-07-02 21:16:57 -0400

    add post /login route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -33,4 +33,14 @@ class UserController < ApplicationController
         end
     end
 
+    post '/login' do
+        user = User.find_by(username: params[:user][:username])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect "/users/#{user.slug}"
+        else
+            redirect '/login'
+        end
+    end
+
 end
