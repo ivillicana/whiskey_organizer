41f150c0dab68acfa45b6dda7a2796664a58c939
Isaac Villicana <villicana.i@gmail.com>
2018-07-03 14:00:15 -0400

    update /whiskeys route and whiskeys/show.erb

--- app/controllers/whiskey_controller.rb
+++ app/controllers/whiskey_controller.rb
@@ -1,7 +1,13 @@
 class WhiskeyController <  ApplicationController
 
     get '/whiskeys' do
-        erb :'show'
+        if logged_in?
+            @user = User.find(session[:user_id])
+            @whiskeys = Whiskey.all
+            erb :'/whiskeys/show'
+        else
+            redirect '/login'
+        end
     end
 
 end
