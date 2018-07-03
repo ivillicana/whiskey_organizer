03e0b349849046da0963048ffa9819464773a507
Isaac Villicana <villicana.i@gmail.com>
2018-07-03 11:53:58 -0400

    add post /items/:id route to ItemController

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -38,4 +38,25 @@ class ItemController < ApplicationController
         end
     end
 
+    post '/items/:id' do
+        if Helper.logged_in?(session)
+            item = Item.find(params[:id])
+            user = Helper.current_user(session)
+            if item.user = user
+                binding.pry
+                if !params[:item][:last_tasted_date].empty?
+                    item.last_tasted_date = params[:item][:last_tasted_date]
+                    item.save
+                    redirect "/items/#{item.id}"
+                else
+                    redirect "/items/#{item.id}/edit"
+                end
+            else
+                redirect '/items'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
 end
