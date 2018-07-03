0801a6569f694ca91dc195d3f284192e8dba23dc
Isaac Villicana <villicana.i@gmail.com>
2018-07-03 10:50:28 -0400

    update /items route

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,8 +1,9 @@
 class ItemController < ApplicationController
 
     get '/items' do
-        if Helper.logged_in(session)
+        if Helper.logged_in?(session)
             @user = User.find(session[:user_id])
+            @items = @user.items
             erb :'/items/show'
         else
             redirect '/login'
