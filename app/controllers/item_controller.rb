2eb67ff8b1c2d3abf76d19434946fd7a14c6adc8
Isaac Villicana <villicana.i@gmail.com>
2018-07-03 11:30:15 -0400

    add /items/:id/edit route to ItemController

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -12,11 +12,24 @@ class ItemController < ApplicationController
 
     get '/items/:id' do
         if Helper.logged_in?(session)
-            binding.pry
             @item = Item.find(params[:id])
             @user = Helper.current_user(session)
             if @item.user == @user
                 erb :'/items/show'
+            else
+                redirect '/items'
+            end
+        else
+            redirect '/login'
+        end
+    end
+
+    get '/items/:id/edit' do
+        if Helper.logged_in?(session)
+            @item = Item.find(params[:id])
+            @user = Helper.current_user(session)
+            if @item.user == @user
+                erb :'/items/edit'
             else
                 redirect '/items'
             end
