ff9ae3ef958a95691a0159c1b0d762e13a87d975
Isaac Villicana <villicana.i@gmail.com>
2018-07-04 11:08:14 -0400

    add input validation to post /items

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -24,17 +24,26 @@ class ItemController < ApplicationController
     post '/items' do
 
         if logged_in?
-            if params[:item][:whiskey_id].empty? && params[:whiskey].all? {|k,v| !v.empty?} && !params[:item][:last_tasted_date].empty?
-                whiskey = Whiskey.create(params[:whiskey])
-                item = Item.create(last_tasted_date: params[:item][:last_tasted_date], whiskey_id: whiskey.id, user_id: current_user.id)
-                redirect "/items/#{item.id}"
-            elsif !params[:item][:whiskey_id].empty? && params[:whiskey].all? {|k,v| v.empty?} && !params[:item][:last_tasted_date].empty?
-                item = Item.create(params[:item])
-                item.user = current_user
-                item.save
-                redirect "/items/#{item.id}"
+            if !params[:item][:last_tasted_date].empty? && params[:item][:last_tasted_date].match?(/^(\d{4}-\d{2}-\d{2})/) #Validates last_tasted_date
+                if params[:item][:whiskey_id].empty? && params[:whiskey].all? {|k,v| !v.empty?}
+                    #create an item with an existing whiskey
+                    whiskey = Whiskey.create(params[:whiskey])
+                    item = Item.create(last_tasted_date: params[:item][:last_tasted_date], whiskey_id:  whiskey.id, user_id: current_user.id)
+                    redirect "/items/#{item.id}"
+                elsif !params[:item][:whiskey_id].empty? && params[:whiskey].all? {|k,v| v.empty?}
+                    #create an item with a new whiskey
+                    item = Item.create(params[:item])
+                    item.user = current_user
+                    item.save
+                    redirect "/items/#{item.id}"
+                else
+                    #error message if both options are selected
+                    flash[:choose_one] = "Please add an existing whiskey OR create a new one"
+                    redirect '/items/new'
+                end
             else
-                flash[:choose_one] = "Please add an existing whiskey OR create a new one"
+                #error message is last_tasted_date is not filled out correctly
+                flash[:date] = "Please add a valid date (e.g. 1964-06-14)"
                 redirect '/items/new'
             end
         else
